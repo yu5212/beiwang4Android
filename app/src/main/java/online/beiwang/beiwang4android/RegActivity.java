72d0a6afece84ac8f6941e2f820544d33c6f1730
yuying <81847062@qq.com>
2016-11-28 10:26:02 +0800

    reg

--- app/src/main/java/online/beiwang/beiwang4android/RegActivity.java
+++ app/src/main/java/online/beiwang/beiwang4android/RegActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class RegActivity extends AppCompatActivity {
@@ -22,6 +24,15 @@ public class RegActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+
+        Menu menu=(Menu)findViewById(R.id.reg_back);
+        menu.findItem(R.id.reg_back).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
             }
         });
     }
